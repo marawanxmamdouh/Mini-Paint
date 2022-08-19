2fea82b61b4fdae3e32f06357bf7742588d8a881
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-19 20:33:48 +0200

    Set up Paint for Drawing

--- app/src/main/java/dev/marawanxmamdouh/minipaint/MyCanvas.kt
+++ app/src/main/java/dev/marawanxmamdouh/minipaint/MyCanvas.kt
@@ -3,14 +3,30 @@ package dev.marawanxmamdouh.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 
 private lateinit var extraCanvas: Canvas
 private lateinit var extraBitmap: Bitmap
+private const val STROKE_WIDTH = 12f
 
 class MyCanvas(context: Context) : View(context) {
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
